a7df99fef0b37dbecf641961bc3e969e83e06e11
Postbird <ptbird@yeah.net>
2019-04-07 19:08:00 +0800

    feat: update error

--- demo2/background.js
+++ demo2/background.js
@@ -7,7 +7,7 @@ chrome.runtime.onInstalled.addListener(() => {
 chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
     chrome.declarativeContent.onPageChanged.addRules([{
         conditions: [new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { hostEquals: 'ptbird.cn' }
+            pageUrl: { hostEquals: 'developer.chrome.com' }
         })
         ],
         actions: [new chrome.declarativeContent.ShowPageAction()]
--- demo2/popup.js
+++ demo2/popup.js
@@ -2,15 +2,15 @@ const changeColor = document.getElementById('changeColor');
 
 chrome.storage.sync.get('color', (data) => {
     changeColor.style.backgroundColor = data.color;
-    changeColor.innerText = data.color;
+    changeColor.setAttribute('value', data.color);
 });
 
-changeColor.onclick = (element) => {
-    const color = element.innerText;
-    chrome.tabs.query({ active: true, currentWindow: truse }, (tabs) => {
-        chrome.tabs.executeScript(
-            tabs[0].id,
-            { code: `document.body.style.backgroundColor = "${color}"` }
-        )
-    })
-}
+changeColor.onclick = function(element) {
+    let color = element.target.value;
+    console.log(color);
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.executeScript(
+          tabs[0].id,
+          {code: 'document.body.style.backgroundColor = "' + color + '";'});
+    });
+  };
